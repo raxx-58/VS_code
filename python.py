cc81ff8a5982788de441ef836eabd397f06543ff
raxx-58 <144779639+raxx-58@users.noreply.github.com>
2025-10-03 20:19:17 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,29 +1,4 @@
 
-print("Hello, World!")
-
-         varaible
-
-            ## String
-first_name = "John"
-last_name = "Doe"
-full_name = first_name + " " + last_name # firstname + lastname to get JohnDoe
-print("Hello " + full_name)
-print(type(full_name)) # to check the type of variable
-            ## int 
-
-age =21 #not in quotes because its integer
-age = age + 1 #age += 1
-print("age")
-print(type(age))    
-print("your age is: " + str(age)) #str() to convert integer to string this is type cast that allowed us to connect them 
-            ## Float
-
-height= 340.33
-print("your height is:" + str(height))
-            ##Bollean only True or False
-human =True
-print(human)
-
 
 
 
